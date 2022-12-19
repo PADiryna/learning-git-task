10b7745f40503a0112c355bf5a0bcf1781074d0c
Iryna Padafet <irini-84@ukr.net>
2022-12-19 02:07:19 +0100

    Complete the script

--- project.py
+++ project.py
@@ -3,4 +3,7 @@ dct = {"bakery": ["bread", "buns","donuts"],
 "vegetable shop" :["carrot", "celery", "arugula"]}
 for k, v in dct.items():
    print("I am going to the", k.capitalize(), "and buy such products", v)
-   
+all = (len(dct["bakery"])+len(dct["vegetable shop"]))
+print("Buy",all,"products together")
+
+
