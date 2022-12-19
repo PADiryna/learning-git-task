d7bcfbd5ff5dda4687d235dcbd4d64ac3247e252
Iryna Padafet <irini-84@ukr.net>
2022-12-19 17:47:31 +0100

    Change values

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 print("Shopping list")
-dct = {"bakery": ["bread", "buns","donuts"],
-"vegetable shop" :["carrot", "celery", "arugula"]}
+dct = {"bakery": ["bread", "buns","cookies"],
+"vegetable shop" :["carrot", "celery", "dill"]}
 for k, v in dct.items():
    print("I am going to the", k.capitalize(), "and buy such products", v)
 all = (len(dct["bakery"])+len(dct["vegetable shop"]))
