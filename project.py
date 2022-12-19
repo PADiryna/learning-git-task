6a7ad70e3361ba78a8965eb8ef63a877de760412
Iryna Padafet <irini-84@ukr.net>
2022-12-19 01:54:20 +0100

    Iterate dict

--- project.py
+++ project.py
@@ -1,3 +1,6 @@
 print("Shopping list")
 dct = {"bakery": ["bread", "buns","donuts"],
 "vegetable shop" :["carrot", "celery", "arugula"]}
+for k, v in dct.items():
+   print("I am going to the", k.capitalize(), "and buy such products", v)
+   
