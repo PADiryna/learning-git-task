40e8101b59264fb67079762285aa70e1ff958964
Iryna Padafet <irini-84@ukr.net>
2022-12-20 11:03:53 +0100

    Make text

--- project.py
+++ project.py
@@ -1,9 +1,1 @@
-print("Shopping list")
-dct = {"bakery": ["buns","cookies"],
-"vegetable shop" :["celery", "dill"]}
-for k, v in dct.items():
-   print("I am going to the", k.capitalize(), "and buy such products", v)
-all = (len(dct["bakery"])+len(dct["vegetable shop"]))
-print("Buy",all,"products together")
-
-
+print("Найщиріші вітання з зимовими святами та найкращі побажання в Новому році!")
